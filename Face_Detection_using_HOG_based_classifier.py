62df875c12072d1f29af307549ce138918e9a33b
Kirti Swagat Mohanty <kirti.kitu@gmail.com>
2020-04-02 23:32:15 +0530

    Update Face_Detection_using_HOG_based_classifier.py

--- Face_Detection_using_HOG_based_classifier.py
+++ Face_Detection_using_HOG_based_classifier.py
@@ -7,27 +7,31 @@ Created on Thu Apr  2 22:17:09 2020
 
 import face_recognition
 from cv2 import imread
+from cv2 import waitKey
+from cv2 import destroyAllWindows
 from cv2 import rectangle
 from cv2 import imwrite
+from cv2 import imshow
 import os
 
 
-path='C:/Users/Documents/Kirti_Swagat/AI/Face_Detection/'
-image_name= 'image.jpg'
+path='C:/Users/NIC/Documents/Kirti_Swagat/AI/Face_Detection/PJ-4_2863_SIEMANL_004_BSC_Q0601_000011/Login/'
+image_name= 'Log_in_10_01_2020.jpg'
 # load the photograph
 image = imread(path+image_name)
 
 faces = face_recognition.face_locations(image)
-#print(faces)
 
-for face in faces:
-     x,y,width,height=face
-     x2,y2= x+width, y+height
-     rectangle(image, (x,y), (x2,y2),(0,0,255),1)
+for (top, right, bottom, left) in faces:
+    rectangle(image, (left, top), (right, bottom), (0, 255, 0),2)
 
 imwrite("./HOG/" + image_name,image)
 file_name= os.path.splitext(image_name)[0]
 output_result= open("./HOG/"+"HOG_Result_" + str(file_name) + ".txt", "w")
 output_result.write("No of Faces Detected Using HOG Method for the image named "+str(file_name)+":"+str(len(faces)))
-output_result.close()   
+output_result.close()
 
+     
+#imshow('Face Detection',image)
+waitKey(0)
+destroyAllWindows()
