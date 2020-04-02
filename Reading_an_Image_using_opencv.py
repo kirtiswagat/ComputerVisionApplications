2d33391c6db5016d6aa5f8806eb73faab5bf5d72
Kirti Swagat Mohanty <kirti.kitu@gmail.com>
2020-04-02 16:05:48 +0530

    Add files via upload

---
+++ Reading_an_Image_using_opencv.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr  2 15:49:02 2020
+
+@author: Kirti Swagat Mohanty
+
+Library Used: OpenCV
+Versino: 4.2.0
+Python Version: 3.7.4
+"""
+
+from cv2 import imread
+from cv2 import imshow
+from cv2 import imwrite
+from cv2 import waitKey
+from cv2 import destroyAllWindows
+
+image= imread('kirti.jpg')
+imwrite('new_image.jpg',image)
+imshow('Test Image',image)
+waitKey(0)
+destroyAllWindows()
